4f05cc76081d782c1169385a13ae1832cc3134e9
caiyue1993 <278060043@qq.com>
2017-01-12 23:59:04 +0800

    Subclass UINavigationBar successfully and ready for next development

--- Tiptoes/Tiptoes/TiptoesNavController.swift
+++ Tiptoes/Tiptoes/TiptoesNavController.swift
@@ -9,27 +9,73 @@
 import UIKit
 
 class TiptoesNavController: UINavigationController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override init(rootViewController: UIViewController) {
+        super.init(navigationBarClass: TiptoesNavBar.self, toolbarClass: nil)
+        viewControllers = [rootViewController]
     }
-    */
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationBar.isHidden = true
+        if let bar = navigationBar as? TiptoesNavBar {
+            view.addSubview(bar.realBar)
+            view.addSubview(bar.barTitle)
+            bar.realBar.frame = CGRect(x: 0, y: view.frame.height - 20, width: view.frame.width, height: 20)
+            bar.barTitle.frame = CGRect(x: 0, y: 300, width: 100, height: 40)
+        }
+    }
+    
+}
 
+extension TiptoesNavController: UINavigationBarDelegate {
+    public func navigationBar(_ navigationBar: UINavigationBar, shouldPush item: UINavigationItem) -> Bool {
+        print(item.title)
+        return true
+    }
+    
+    public func navigationBar(_ navigationBar: UINavigationBar, didPush item: UINavigationItem) {
+         print(item.title)
+    }
+    
+    public func navigationBar(_ navigationBar: UINavigationBar, shouldPop item: UINavigationItem) -> Bool {
+        print(item.title)
+        return true
+    }
+    
+    public func navigationBar(_ navigationBar: UINavigationBar, didPop item: UINavigationItem) {
+        print(item.title)
+    }
+}
+
+class TiptoesNavBar: UINavigationBar {
+    
+    var barTitle: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 8)
+        return label
+    }()
+    
+    var realBar: UIView = {
+        let bar = UIView()
+        bar.backgroundColor = UIColor.lightGray
+        return bar
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
