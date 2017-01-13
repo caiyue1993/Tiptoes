571938aeca211849e4a9119a004c1a37a61b937a
caiyue1993 <278060043@qq.com>
2017-01-13 16:32:14 +0800

    regularize code and add comment

--- Tiptoes/Tiptoes/TiptoesNavController.swift
+++ Tiptoes/Tiptoes/TiptoesNavController.swift
@@ -30,6 +30,8 @@ class TiptoesNavController: UINavigationController {
         }
     }
     
+    private let tiptoesHeight = UIApplication.shared.statusBarFrame.size.height
+    
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
@@ -46,26 +48,26 @@ class TiptoesNavController: UINavigationController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Hide the system navigation bar
         navigationBar.isHidden = true
+        
         if let bar = navigationBar as? TiptoesNavBar {
-            view.addSubview(bar.realBar)
+            view.addSubview(bar.tiptoes)
             view.addSubview(bar.currentTitleLabel)
             view.addSubview(bar.priorTitleLabel)
-            bar.realBar.frame = CGRect(x: 0, y: view.frame.height - 20, width: view.frame.width, height: 20)
-            bar.currentTitleLabel.frame = CGRect(x: 375 / 2, y: view.frame.height - 20, width: 100, height: 20)
-            bar.priorTitleLabel.frame = CGRect(x: 375 / 2, y: view.frame.height - 20, width: 100, height: 20)
+            bar.tiptoes.frame = CGRect(x: 0, y: view.frame.height - tiptoesHeight, width: view.frame.width, height: tiptoesHeight)
+            bar.currentTitleLabel.sizeToFit()
+            bar.currentTitleLabel.center = CGPoint(x: view.frame.width / 2, y: bar.tiptoes.center.y)
+            bar.priorTitleLabel.frame = bar.currentTitleLabel.frame
         }
-        
-        interactivePopGestureRecognizer?.addTarget(self, action: #selector(handleRealBarDisplay))
+        interactivePopGestureRecognizer?.addTarget(self, action: #selector(handleTiptoesDisplay))
     }
     
-    func handleRealBarDisplay() {
-        
+    func handleTiptoesDisplay() {
+        // You can customize the transition style here
         let currentAlpha = (interactivePopGestureRecognizer?.location(in: view).x)! / view.frame.width
-        
         barCurrentTitleLabel.alpha = 1 - currentAlpha
         barPriorTitleLabel.alpha = currentAlpha
-        
     }
     
 }
@@ -73,35 +75,42 @@ class TiptoesNavController: UINavigationController {
 extension TiptoesNavController: UINavigationBarDelegate {
     
     public func navigationBar(_ navigationBar: UINavigationBar, shouldPush item: UINavigationItem) -> Bool {
+        barPriorTitleLabel.text = ""
+        barPriorTitleLabel.isHidden = false
         return true
     }
     
     public func navigationBar(_ navigationBar: UINavigationBar, didPush item: UINavigationItem) {
-        
         if let pushTitle = item.title {
             barCurrentTitleLabel.text = pushTitle
+            barCurrentTitleLabel.sizeToFit()
+            // TO DO: put label to right center position, now if the title is too long we can find that label is not at the center
         }
     }
     
     public func navigationBar(_ navigationBar: UINavigationBar, shouldPop item: UINavigationItem) -> Bool {
         barPriorTitleLabel.isHidden = false
-        if let popTitle = viewControllers[0].title {
-            barPriorTitleLabel.text = popTitle
+        // Get the former viewcontroller
+        let numberOfViewControllers = viewControllers.count
+        if numberOfViewControllers > 0 {
+            if let popTitle = viewControllers[numberOfViewControllers - 1].title {
+                barPriorTitleLabel.text = popTitle
+            }
         }
         return true
     }
     
+    // The status of completion of pop
     public func navigationBar(_ navigationBar: UINavigationBar, didPop item: UINavigationItem) {
         barCurrentTitleLabel.text = barPriorTitleLabel.text
         barCurrentTitleLabel.alpha = 1.0
-        
         barPriorTitleLabel.isHidden = true
-        
     }
 }
 
 class TiptoesNavBar: UINavigationBar {
     
+    /// 用来展示当前 viewController 的 title
     var currentTitleLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 8)
@@ -110,6 +119,7 @@ class TiptoesNavBar: UINavigationBar {
         return label
     }()
     
+    /// 用来展示前一页 viewController 的 title
     var priorTitleLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 8)
@@ -117,10 +127,10 @@ class TiptoesNavBar: UINavigationBar {
         return label
     }()
     
-    var realBar: UIView = {
-        let bar = UIView()
-        bar.backgroundColor = UIColor.lightGray
-        return bar
+    var tiptoes: UIView = {
+        let toes = UIView()
+        toes.backgroundColor = UIColor.lightGray
+        return toes
     }()
     
     override init(frame: CGRect) {
